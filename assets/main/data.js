51968004013935c68b3c2d0660a26e101dbf11cc
Liu Chao <liuchao36@gmail.com>
2011-07-25 15:52:17 +0800

    Launch function

--- assets/main/data.js
+++ assets/main/data.js
@@ -2,13 +2,13 @@ APPStore = function(){
 
 };
 
-APPStore.APPSTORE_DOMAIN='http://localhost:8080/appstore';
+APPStore.APPSTORE_DOMAIN='http://10.0.2.2:8080/appstore';
 APPStore.APPSTORE_LIST_URL=APPStore.APPSTORE_DOMAIN+'/webapp/list.json';
 
 APPStore.prototype.load = function(callback){
     $.getJSON(APPStore.APPSTORE_LIST_URL, function(data){
-        alert(data);
-        callback(data);
+        if(data.apps)
+            callback(data.apps);
     });
 
 };
